ab31c5c15399d360d93a568521387cbc521759cb
edem4ik322 <rustemov.edem@mail.ru>
2025-01-18 23:03:02 +0300

    tv_7_3

---
+++ models/tv.js
@@ -0,0 +1,17 @@
+var mongoose = require('mongoose')
+var Schema = mongoose.Schema
+var tvSchema = new Schema({
+title: String,
+nick: {
+type: String,
+unique: true,
+required: true
+},
+avatar: String,
+desc: String,
+created:{
+type:Date,
+default:Date.now
+}
+})
+module.exports.TV = mongoose.model("TV", tvSchema)
--- testMongoose.js
+++ testMongoose.js
@@ -1,12 +1,8 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://127.0.0.1:27017/testMongoose2024');
-
-var schema = mongoose.Schema({ name: String })
-
-schema.methods.on = function(){
-    console.log(this.name + " включился")
-    }
-
-const tv = mongoose.model('Tv', schema);
-const sber65 = new tv({ name: 'Sber QLED 4K UHD 65' });
-sber65.save().then(() => sber65.on());
+var TV = require('./models/tv.js').TV
+var tv = new TV({
+title: "Sber QLED 4K UHD 65",
+nick: "Sber-QLED-4K-UHD-65",
+})
+tv.save();
