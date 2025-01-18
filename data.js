e40d8eba0376f7c60892544042540291780613dc
edem4ik322 <rustemov.edem@mail.ru>
2025-01-18 22:16:20 +0300

    tv_6_3

--- data.js
+++ data.js
@@ -1,26 +1,32 @@
 var data = [{
-    title: 'Карамелька',
-    nick: 'karamelka',
-    avatar: '/images/karamelka.jpeg',
-    desc: 'Cамый младший член семьи. Коронная фраза — «Я знаю, что
-    надо делать!». Носит красный бант и красное платье.'
+    title: 'Sber QLED 4K UHD 65',
+    nick: 'Sber-QLED-4K-UHD-65',
+    avatar: 'images/Sber-QLED-4K-UHD-65.avif',
+    desc: 'Продвинутый телевизор от отечественного бренда Sber'
     },
     {
-    title: 'Компот',
-    nick: 'kompot',
-    avatar: '/images/kompont.png',
-    desc: 'Cтарший котёнок в семье. Ходит в школу. Очень любит
-    грибы, читает про них энциклопедии. Любит игры на логику. Когда
-    очень сильно огорчается, восклицает «Ну вот!» и начинает плакать.
-    Обожает вкусно поесть. Носит зелёный костюм и шапку.'
+    title: 'Яндекс ТВ Станция Про с Алисой 55',
+    nick: 'Yandex-TV-Pro-with-Alice-55',
+    avatar: 'images/Yandex-TV-Pro-with-Alice-55.jpg',
+    desc: 'Это умный телевизор от Яндекса, который может управляться при помощи голосового ассистента Алиса и тесно связан с нейросетью YandexGPT.'
     },
     {
-    title: 'Коржик',
-    nick: 'korzhik',
-    avatar: '/images/korzhik.png',
-    desc: 'Средний котёнок в семье. Очень любит футбол. Любит
-    бегать, прыгать и веселиться. Иногда делает такие вещи, что из них
-    приходится выбираться всей семье. Одет в форму моряка.'
+    title: 'Samsung UE50CU8000UXRU',
+    nick: 'Samsung-UE50CU8000UXRU',
+    avatar: 'images/Samsung-UE50CU8000UXRU.jpeg',
+    desc: 'Телевизор от проверенного бренда Samsung, который поддерживает технологию HDR10.'
+    },
+    {
+        title: 'TCL 55 55P635 4K LCD',
+        nick: 'TCL-55-55P635-4K-LCD',
+        avatar: 'images/TCL-55-55P635-4K-LCD.jpeg',
+        desc: 'Модель от известного и уважаемого бренда TCL, которая может похвастаться мощным процессором (AIPQ 2.0), поддержкой HDR10 и матрицей VA с хорошими углами обзора.'
+    },
+    {
+        title: 'Haier 55 Smart TV S3 RU',
+        nick: 'Haier-55-Smart-TV-S3-RU',
+        avatar: 'images/Haier-55-Smart-TV-S3-RU.jpeg',
+        desc: 'Превосходный по качеству 55-дюймовый телевизор с отличным качеством картинки.'
     }
     ];
     module.exports.data = data;
