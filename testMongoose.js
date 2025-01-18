d60eb7b961e68823ecd1f6cd037aa6bfae9c42ee
edem4ik322 <rustemov.edem@mail.ru>
2025-01-18 22:41:52 +0300

    tv_7_2

--- testMongoose.js
+++ testMongoose.js
@@ -1,5 +1,12 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://127.0.0.1:27017/testMongoose2024');
-const tv = mongoose.model('Tv', { name: String });
+
+var schema = mongoose.Schema({ name: String })
+
+schema.methods.on = function(){
+    console.log(this.name + " включился")
+    }
+
+const tv = mongoose.model('Tv', schema);
 const sber65 = new tv({ name: 'Sber QLED 4K UHD 65' });
-sber65.save().then(() => console.log('Телевизор Sber QLED 4K UHD 65'));
+sber65.save().then(() => sber65.on());
