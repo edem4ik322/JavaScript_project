3681b6552096c88359e84104fde040fd11ef258b
edem4ik322 <rustemov.edem@mail.ru>
2025-01-19 16:10:04 +0300

    tv_10_6

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ router.get('/', function(req, res, next) {
 });
 /* GET login/registration page. */
 router.get('/logreg', function(req, res, next) {
-  res.render('logreg',{title: 'Вход'});
+  res.render('logreg',{title: 'Вход', error: null});
   });
   router.post('/logreg', async function(req, res, next) {
     var username = req.body.username
@@ -31,7 +31,7 @@ router.get('/logreg', function(req, res, next) {
     req.session.user_id = foundUser._id
     res.redirect('/')
     } else {
-    res.render('logreg',{title: 'Вход'});
+    res.render('logreg',{title: 'Вход', error: 'Пароль не верный'});
     }
     }
 });
