03ffe0fdcdd2cb91306a6d1ad40c08d27c899352
edem4ik322 <rustemov.edem@mail.ru>
2025-01-19 14:35:08 +0300

    tv_9_5

--- app.js
+++ app.js
@@ -42,7 +42,8 @@ app.use(function(req,res,next){
   req.session.counter = req.session.counter + 1 || 1
   next()
   })
-  
+  app.use(require("./middlewares/createMenu.js"))
+
 
 app.use(session({
   secret: "TV",
