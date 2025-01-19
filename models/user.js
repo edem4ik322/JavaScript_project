17efeaaae036e6b2bf9389c3eecc52ba3bd5a9c8
edem4ik322 <rustemov.edem@mail.ru>
2025-01-19 15:14:01 +0300

    tv_10_4

--- models/user.js
+++ models/user.js
@@ -1,6 +1,8 @@
 var mongoose = require('mongoose')
-var crypto = require("crypto")
 var Schema = mongoose.Schema
+var crypto = require("crypto")
+
+
 var userSchema = new Schema({
 username:{
 type: String,
@@ -20,17 +22,17 @@ type:Date,
 default: Date.now
 }
 })
-
 userSchema.virtual("password").set(function(password){
     this._purePassword = password
     this.salt = Math.random() + ""
     this.hashedPassword = this.encryptPassword(password)
-    }).get(function(){
+}).get(function(){
     return this._purePassword
-    })
-    userSchema.methods.encryptPassword = function(password){
+})
+userSchema.methods.encryptPassword = function(password){
     return crypto.createHmac('sha1', this.salt).update(password).digest('hex')
-    }
-    
-
+}
+userSchema.methods.checkPassword = function(password){
+    return this.encryptPassword(password) === this.hashedPassword
+}
 module.exports.User = mongoose.model("User", userSchema)
