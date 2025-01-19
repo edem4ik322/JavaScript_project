72699ff8e5f2e8c2b09ab9d13bb7f85a6507c388
edem4ik322 <rustemov.edem@mail.ru>
2025-01-19 14:50:11 +0300

    tv_10_1

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,10 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express', counter:req.session.counter });
 });
 
+/* GET login/registration page. */
+router.get('/logreg', function(req, res, next) {
+  res.render('logreg',{title: 'Вход'});
+  });
+  
+
 module.exports = router;
